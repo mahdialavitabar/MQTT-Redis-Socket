5a2deb75d907faf15b60fc9824f0a607cdd42aa8
davioncoder <130570744+davioncoder@users.noreply.github.com>
2023-06-18 14:08:15 +0330

    name fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Mqtt from "@/app/logs";
+import Logs from "@/app/logs";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <Mqtt/>
+      <Logs/>
     </main>
   )
 }
